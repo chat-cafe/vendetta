c9e4e15d326b1b8703705384e5f5bb470eab665c
Jake <68775263+jb0s@users.noreply.github.com>
2023-07-12 18:54:34 +0200

    Update you.tsx

--- src/ui/settings/patches/you.tsx
+++ src/ui/settings/patches/you.tsx
@@ -21,8 +21,8 @@ export default function patchYou() {
         ret.splice(1, 0, cafe.layout);
 
         // Add our settings
-        const accountSettingsIndex = ret.findIndex((i: any) => i.title === i18n.Messages.ACCOUNT_SETTINGS);
-        ret.splice(accountSettingsIndex + 1, 0, data.layout);
+        //const accountSettingsIndex = ret.findIndex((i: any) => i.title === i18n.Messages.ACCOUNT_SETTINGS);
+        //ret.splice(accountSettingsIndex + 1, 0, data.layout);
 
         // Upload Logs button be gone
         const supportCategory = ret.find((i: any) => i.title === i18n.Messages.SUPPORT);
