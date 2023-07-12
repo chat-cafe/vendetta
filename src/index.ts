4ccb8d71979f7a4810f22c6ecf7171808bf8141c
Jake <68775263+jb0s@users.noreply.github.com>
2023-07-12 18:34:27 +0200

    testing

--- src/index.ts
+++ src/index.ts
@@ -13,45 +13,9 @@ import { showToast } from "@ui/toasts";
 import { BundleUpdaterManager } from "@lib/native";
 
 export const __cafe_mod_version = "1.0.0";
-export let global_env_patched = false;
 
 export default async () => {
     logger.log(`Cafe mod v${__cafe_mod_version}`);
-
-    // Override global env
-    if(!global_env_patched) {
-        BundleUpdaterManager.reload()
-        window.GLOBAL_ENV = {
-            "API_ENDPOINT": "//cafe.jb0s.dev/api",
-            "API_VERSION": 9,
-            "GATEWAY_ENDPOINT": "wss://cafe.jb0s.dev/gateway",
-            "WEBAPP_ENDPOINT": "//cafe.jb0s.dev",
-            "CDN_HOST": "cafe.jb0s.dev",
-            "ASSET_ENDPOINT": "//canary.discord.com",
-            "MEDIA_PROXY_ENDPOINT": "//media.discordapp.net",
-            "WIDGET_ENDPOINT": "//cafe.jb0s.dev/widget",
-            "INVITE_HOST": "discord.gg",
-            "GUILD_TEMPLATE_HOST": "discord.new",
-            "GIFT_CODE_HOST": "discord.gift",
-            "RELEASE_CHANNEL": "cafe",
-            "MARKETING_ENDPOINT": "//cafe.jb0s.dev",
-            "BRAINTREE_KEY": null,
-            "STRIPE_KEY": null,
-            "NETWORKING_ENDPOINT": "//router.discordapp.net",
-            "RTC_LATENCY_ENDPOINT": "//latency.discord.media/rtc",
-            "ACTIVITY_APPLICATION_HOST": "discordsays.com",
-            "PROJECT_ENV": "production",
-            "REMOTE_AUTH_ENDPOINT": "//remote-auth-gateway.discord.gg",
-            "SENTRY_TAGS": null,
-            "MIGRATION_SOURCE_ORIGIN": "https://canary.discordapp.com",
-            "MIGRATION_DESTINATION_ORIGIN": "https://cafe.jb0s.dev",
-            "HTML_TIMESTAMP": 1688857866123,
-            "ALGOLIA_KEY": null,
-            "PUBLIC_PATH": "/assets/"
-        };
-
-        global_env_patched = true;
-    }
 
     // Load everything in parallel
     const unloads = await Promise.all([
